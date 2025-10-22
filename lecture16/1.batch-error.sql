f9ea4bf11ef10da22ab54a446df1c1fb7a8f7727
alikhawaja <ali.khawaja@outlook.com>
2025-10-22 17:09:55 +0500

    lecture 16 updates

--- lecture16/1.batch-error.sql
+++ lecture16/1.batch-error.sql
@@ -1,8 +1,33 @@
+if OBJECT_ID('NewTable') IS NOT NULL
+    DROP TABLE NewTable;
+
 CREATE TABLE NewTable (Id INT PRIMARY KEY, Info CHAR(3));
 GO
+
 INSERT INTO NewTable VALUES (1, 'aaa');
 INSERT INTO NewTable VALUES (2, 'bbb');
 INSERT INTO NewTable VALUSE (3, 'ccc');  -- Syntax error.
 GO
+
+SELECT * FROM NewTable;  -- Returns no rows.
+GO
+
+
+INSERT INTO NewTable VALUES (1, 'aaa');
+INSERT INTO NewTable VALUES (2, 'bbb');
+INSERT INTO NewTable VALUES (3, 'ccc');
+GO
+
+SELECT * FROM NewTable;  -- Returns no rows.
+GO
+
+SET XACT_ABORT ON;
+GO
+
+INSERT INTO NewTable VALUES (1, 'aaa');
+INSERT INTO NewTable VALUES (2, 'bbb');
+INSERT INTO NewTable VALUES (4, 'ccc');
+GO
+
 SELECT * FROM NewTable;  -- Returns no rows.
 GO
--- lecture16/2.save-point.sql
+++ lecture16/2.save-point.sql
@@ -1,3 +1,6 @@
+if(OBJECT_ID('TestTable') IS NOT NULL)
+    DROP TABLE TestTable;
+
 CREATE TABLE TestTable
 (
    ID INT NOT NULL,
@@ -5,17 +8,20 @@ CREATE TABLE TestTable
    PRIMARY KEY (ID)
 )
 GO
+
 BEGIN TRANSACTION 
  
    INSERT INTO TestTable( ID, Value )
    VALUES  ( 1, N'10')
    -- this will create a savepoint after the first INSERT
    SAVE TRANSACTION FirstInsert
+
    INSERT INTO TestTable( ID, Value )
    VALUES  ( 2, N'20')
  
    -- this will rollback to the savepoint right after the first INSERT was done
    ROLLBACK TRANSACTION FirstInsert
+   
 -- this will commit the transaction leaving just the first INSERT 
 COMMIT
  
