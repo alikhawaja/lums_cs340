58f5c42e595cd88840347be14a1b186033f9813e
Ali Khawaja <akhawaja@microsoft.com>
2025-10-22 13:16:38 +0500

    lecture 16 changes

---
+++ lecture16/2.save-point.sql
@@ -0,0 +1,22 @@
+CREATE TABLE TestTable
+(
+   ID INT NOT NULL,
+   Value INT NOT NULL,
+   PRIMARY KEY (ID)
+)
+GO
+BEGIN TRANSACTION 
+ 
+   INSERT INTO TestTable( ID, Value )
+   VALUES  ( 1, N'10')
+   -- this will create a savepoint after the first INSERT
+   SAVE TRANSACTION FirstInsert
+   INSERT INTO TestTable( ID, Value )
+   VALUES  ( 2, N'20')
+ 
+   -- this will rollback to the savepoint right after the first INSERT was done
+   ROLLBACK TRANSACTION FirstInsert
+-- this will commit the transaction leaving just the first INSERT 
+COMMIT
+ 
+SELECT * FROM TestTable
